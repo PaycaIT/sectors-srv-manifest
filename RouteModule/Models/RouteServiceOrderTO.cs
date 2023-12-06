a0ac1bc3a631cb1c01ef9f332d2140f594ba802f
Benjamin Belmar <benjamin.belmar@usach.cl>
2023-12-06 18:08:32 -0300

    feat: new route model

--- RouteModule/Models/RouteServiceOrderTO.cs
+++ RouteModule/Models/RouteServiceOrderTO.cs
@@ -11,3 +11,15 @@ public class RouteServiceOrderTO
     public string RecipientPhone { get; set; } = "";
     public string ReferenceDir { get; set; } = "Por implementar";
 }
+
+public class DetailedRoutesData
+{
+    public int Id { get; set; }
+    public int ServiceOrderId { get; set; }
+    public string ExternalId { get; set; } = "";
+    public string Client { get; set; } = "";
+    public string RecipientAddress { get; set; } = "";
+    public string RecipientName { get; set; } = "";
+    public string RecipientPhone { get; set; } = "";
+    public string ReferenceDir { get; set; } = "Por implementar";
+}
